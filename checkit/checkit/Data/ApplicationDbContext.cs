d5b13342a9683ae941115aa68daaf990a80ad091
yousiftalaq <87570362+yousiftalaq@users.noreply.github.com>
2021-11-14 17:16:37 +0300

    Add files via upload

---
+++ checkit/checkit/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using InAndOut.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace checkit.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<ExpenseType> ExpenseTypes { set; get; }
+    }
+}
