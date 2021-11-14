39164938e03ba6bb2e0616a8f561917e78e7b052
yousiftalaq <87570362+yousiftalaq@users.noreply.github.com>
2021-11-14 17:18:05 +0300

    Add files via upload

---
+++ checkit/checkit/Services/EmailSender.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace checkit.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            using (var message = new MailMessage())
+            {
+                message.To.Add(new MailAddress(email, email));
+                message.From = new MailAddress("yousiftalaq1998@gmail.com", "Rest Password");
+                message.Subject = subject;
+                message.Body = htmlMessage;
+                message.IsBodyHtml = true;
+
+                using (var client = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    client.Port = 587;
+                    client.UseDefaultCredentials = false;
+                    client.Credentials = new NetworkCredential("yousiftalaq1998@gmail.com", "Rest Password");
+                    client.EnableSsl = true;
+                    client.Send(message); 
+                }
+            }
+
+
+            return Task.CompletedTask;
+          }
+        }
+}
+
